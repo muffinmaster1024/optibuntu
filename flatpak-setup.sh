078cf8396881ce4b859bf4b8881197a13dcfd057
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-04-22 18:03:12 +0200

    Create flatpak-setup.sh

---
+++ flatpak-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:alexlarsson/flatpak
+sudo apt update
+sudo apt install flatpak -y
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "You should reboot now for the setup to be completed"
