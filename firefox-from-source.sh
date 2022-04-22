b5d5f88a89c79bfc8a0da45081da69e4916026d0
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-04-22 17:56:45 +0200

    Create firefox-from-source.sh

---
+++ firefox-from-source.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#This script basically is taken from https://support.mozilla.org/en-US/kb/install-firefox-linux
+
+#Download latest release
+wget "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US" -O ~/Downloads/latest-firefox.tar.bz2
+
+#Unpacking
+tar xjf lastest-firefox.tar.bz2
+
+#Move to /opt
+sudo mv firefox /opt
+
+#linking to /bin
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+
+#creating desktop file
+sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+
+#Removing Download
+rm ~/Downloads/latest-firefox.tar.bz2
