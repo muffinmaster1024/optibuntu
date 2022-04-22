4c9f76e6d89e868c2793d8db7c4ed2062a4917f7
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-04-22 23:11:15 +0200

    Create setup-element.sh

---
+++ setup-element.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#from https://element.io/get-started#linux-details
+
+sudo apt install -y wget apt-transport-https
+‍
+sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+‍
+echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+
+if ! [[ "$1" == "-m" ]]; then
+    sudo apt update && sudo apt install element-desktop -y
+fi
