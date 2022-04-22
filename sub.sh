e8b2253132deff469de53b85c431397e26e5ba74
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-04-22 23:17:08 +0200

    Create sub.sh

---
+++ sub.sh
@@ -0,0 +1,13 @@
+#Here you can add other notes for system modifications
+echo "Gnome Plugins:"
+echo ""
+
+echo "PWA:"
+echo ""
+
+echo "AppImages:"
+echo ""
+
+echo "Others: "
+echo "https://www.torproject.org/download/"
+echo "https://protonvpn.com/support/linux-vpn-setup/" 
