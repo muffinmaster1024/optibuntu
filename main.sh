7b1a0d6ba4a8747e642a5547d5b5d1c582e55168
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-04-22 19:56:44 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,24 +2,32 @@
 
 ./no-snap.sh
 
-#PPA
-sudo add-apt-repository ppa:alexlarsson/flatpak
-# sudo add-apt-repository ppa:PACKAGE
+#PPAs from ppa.lst
+for ppa in $(cat ppa.lst)
+do
+    sudo add-apt-repository ppa:$ppa
+done
+
+./setup-signal.sh -m
+./setup-brave-browser.sh -m
 
 sudo apt update
-sudo apt upgrade -y
+#sudo apt upgrade -y
 
-#APT
-sudo apt install dconf-editor gnome-tweaks #engrampa
-sudo apt install flatpak gnome-software-plugin-flatpak gnome-software
-#sudo apt remove file-roller
+#APT from apt.lst
+for apt in $(cat apt.lst)
+do
+    sudo install -y $apt
+done
 
-./firefox-from-source.sh
-./setup-signal.sh
-./setup-brave-browser.sh
 #./sub.sh
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-#FLAT
-#flatpak install PACKAGE
+#FLATPAKs from flatpak .lst
+for flat in $(cat flat.lst)
+do
+    flatpk install $flat
+done
+
+./firefox-from-source.sh
