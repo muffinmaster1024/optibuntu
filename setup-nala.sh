5c34d9529b16f004c61bbc074709a26ce6282640
muffinmaster1024 <59618322+muffinmaster1024@users.noreply.github.com>
2022-07-23 21:11:38 +0200

    Update setup-nala.sh

--- setup-nala.sh
+++ setup-nala.sh
@@ -3,11 +3,19 @@
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 
+OSLEVEL=`cat /etc/os-release|grep VERSION_ID|cut -d'"' -f2|cut -d'.' -f1`
+
 if ! [[ "$1" == "-m" ]]; then
     sudo apt update && \
-    if [[ `cat /etc/os-release|grep VERSION_ID|cut -d'"' -f2|cut -d'.' -f1` -lt "22" ]]; then
+    if [[ $OSLEVEL -lt "22" ]]; then
         sudo apt install nala-legacy -y
     else
         sudo apt install nala -y
     fi
+else
+    if [[ $OSLEVEL -lt "22" ]]; then
+        sed -i 's/$/ nala-legacy/' apt.lst
+    else
+        sed -i 's/$/ nala/' apt.lst
+    fi    
 fi
